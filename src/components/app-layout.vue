6072558f7a85e3fb891793f2a6faa90cb8d6bd32
maywzp <maywzp@foxmail.com>
2022-06-22 16:49:44 +0800

    tailwindcss

--- src/components/app-layout.vue
+++ src/components/app-layout.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <h1>layout</h1>
-    <router-view />
+    <app-header></app-header>
+    <main class="app-main">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import AppHeader from './app-header.vue'
+
+export default {
+  components: { AppHeader },
+}
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.app-main {
+  padding-top: 70px;
+}
+</style>
